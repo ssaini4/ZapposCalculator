a1975cedd9c28643483b52a3fc5c5c42a56a323e
ssaini4 <ssaini4@illinois.edu>
2016-02-04 22:22:38 -0600

    Initial Commit

---
+++ zapposCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  zapposCalculator
+//
+//  Created by Saksham Saini on 2/4/16.
+//  Copyright © 2016 Saksham_Saini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ zapposCalculator/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  zapposCalculator
+//
+//  Created by Saksham Saini on 2/4/16.
+//  Copyright © 2016 Saksham_Saini. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
